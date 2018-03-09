725319350d527c4647325a3be8469e76336c1a72
quan tran <tranmqt@gmail.com>
2018-03-08 17:31:37 -0800

    added sequence wrapper and imperative timed sequence runner

--- main.js
+++ main.js
@@ -17,3 +17,27 @@ JSequence.timedSequencer = function (time) {
         }
     }
 }
+
+/**
+ * Executes an array of actions at constant time intervals.
+ *
+ * @param {number} time - the time to pause between actions
+ * @param {Array} actions - an array of functions to execute sequentially
+ */
+JSequence.timedSequence = function (time, actions) {
+    this.timedSequencer(time)(actions)
+}
+
+/**
+ * Create a sequence of actions to execute at a later time.
+ *
+ * @param {number} actions - an array of functions to execute sequentially
+ * @param {Object} - a sequence object that 
+ */
+JSequence.createSequence = function (actions) {
+    return {
+        execute: function (time) {
+            JSequence.timedSequencer(time, actions)
+        }
+    }
+}
